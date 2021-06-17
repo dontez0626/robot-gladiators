4d2e1212c306a5801730bfe13daa689ccdcd4108
G.Parker <82189292+dontez0626@users.noreply.github.com>
2021-06-17 15:08:54 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,12 @@ var playerMoney =10;
 console.log(playerName, playerAttack, playerHealth);
 
 
-var enemyName = "Roborto";
+var enemyNames = [ "Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+enemyNames[enemyNames.length];
 
-var fight = function() {
+var fight = function(enemyNames) {
   //Alert players that they are starting a the round
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -21,19 +22,19 @@ var fight = function() {
 // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining. "
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died! ");
+      window.alert(enemyNames + " has died! ");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left. ");
     }
       // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attackecd " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attackecd " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
     
 // check player's health
@@ -59,10 +60,13 @@ var fight = function() {
 } else {
   window.alert("You need to choose a valid option. Try again!");
 }
+
 }
   
 
 
    
 
-   fight();
+   for(var i = 0; i < enemyNames.length; i++) {
+     fight(enemyNames[i]);
+   }
